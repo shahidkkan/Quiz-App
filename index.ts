e2cc85854093e8d925c56734e9a0bc6ee0100795
Shahid khan <161056935+shahidkkan@users.noreply.github.com>
2024-05-09 23:58:40 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.yellowBright.italic.bold("\n\t''''Welcome to the Quiz App''''\t\n"));
+
+const apiLink: string = "https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=multiple";
+
+let fetchData = async(data: string) => {
+    let fetchQuiz:any = await fetch(data);
+    let res = await fetchQuiz.json();
+    return res.results;
+};
+let apiData = await fetchData(apiLink);
+
+let startQuiz = async() => {
+    let score:number = 0;
+    let userName = await inquirer.prompt({
+        type: "input",
+        name: "username",
+        message: chalk.blue.bold("What is your Name?")
+    });
+
+for(let i=1 ; i<6 ; i++){
+    let answers = [...apiData[i].incorrect_answers, apiData[i].correct_answer];
+    let answer = await inquirer.prompt({
+        type: "list",
+        name: "quiz",
+        message: chalk.redBright(apiData[i].question),
+        choices: answers.map((val:any) => val)
+    });
+    if(answer.quiz == apiData[i].correct_answer){
+        ++score;
+        console.log(chalk.yellowBright.bold("Correct"));
+    } else {
+        console.log(chalk.magentaBright.bold(`Correct answer is ${apiData[i].correct_answer}`));
+    }
+};
+console.log(chalk.cyanBright.bold.italic(`\n\tDear ${userName.username}, your score is ${score} out of ${'5'}\t\n`));
+};
+
+
+async function startAgain(){
+    do{
+        await startQuiz();
+        var again = await inquirer.prompt({
+            type: "input",
+            name: "restart",
+            message: chalk.greenBright.bold("Do you want to Continue? Press Y or N :")
+        })
+
+        
+    } while(again.restart == "y" || again.restart == "Y" || again.restart == "yes" || again.restart == "YES");
+    
+};
+startAgain();
